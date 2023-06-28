05d05d732a52fcf4990ec4873cc0e38565efb0f6
anjali bajantri <anjali.bajantri@mobileprogramming.com>
2023-06-28 15:35:24 +0530

    Update Home Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div  style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
         <h3 style={{color: "#000", textAlign:"center", fontSize : "36px" }}>My React Website</h3>
+        <h3 style={{color: "#000", textAlign:"center", fontSize : "30px" }}>Welcome to the website</h3>
     </div>
   )
 }
